bf8f83f4953cf737fe5269b50f8f78f4d4eac1b7
Leg3ndEntity <simone.sarnataro02@gmail.com>
2024-02-16 09:49:49 +0100

    creazione file

---
+++ Toothless/Toothless/ToothlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToothlessApp.swift
+//  Toothless
+//
+//  Created by Simone Sarnataro on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ToothlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
