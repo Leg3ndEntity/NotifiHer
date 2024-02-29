1646cc6937892bd5de37ffd33c3e79c8880be204
andrearomy <andry21yt@gmail.com>
2024-02-29 09:25:44 +0100

    Firebase structure

--- Toothless/Toothless/ToothlessApp.swift
+++ Toothless/Toothless/ToothlessApp.swift
@@ -9,13 +9,13 @@ import SwiftUI
 import PushToTalk
 import AVFoundation
 import UIKit
+import Firebase
 
 
 @main
 struct Toothless_provaApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var viewModel = MapViewModel()
-    
-    
     
     var body: some Scene {
         
@@ -23,16 +23,6 @@ struct Toothless_provaApp: App {
             Toothless()
                 .onAppear{viewModel.checkIfLocationEnabled()
                 }
-                .onAppear {
-                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-                        if granted {
-                            print("Autorizzazione alle notifiche ottenuta")
-                        } else {
-                            print("Autorizzazione alle notifiche negata")
-                        }
-                    }
-                }
-            
                 .task {
                     // Initialize the PushToTalk Manager
                 }
@@ -40,3 +30,84 @@ struct Toothless_provaApp: App {
         
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate{
+    
+    let gcmMessageIDKey = "gcm.message_id"
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        Messaging.messaging().delegate = self
+        
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        return true
+    }
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: 
+    [AnyHashable : Any],
+                     fetchCompletionHandler completionHandler: @escaping
+    (UIBackgroundFetchResult) -> Void) {
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print(userInfo)
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+    }
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        
+        let dataDict:[String: String] = ["token": fcmToken ?? ""]
+print(dataDict)
+    }
+}
+
+@available(iOS 10.0, *)
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print(userInfo)
+        
+        completionHandler([.banner, .banner, .sound])
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print(userInfo)
+        
+        completionHandler()
+    }
+}
+
