386c72b4900ceedf333d334c49d98f2f84813bb1
Leg3ndEntity <simone.sarnataro02@gmail.com>
2024-03-06 13:47:16 +0100

    all fixed (welcomeView)

--- Toothless/Toothless/Views/OnBoarding/SignUpView.swift
+++ Toothless/Toothless/Views/OnBoarding/SignUpView.swift
@@ -13,18 +13,18 @@ struct SignupView: View {
     @State private var surname: String = ""
     @State private var generatedCode: String = ""
     let customDocumentID = "customDocumentID123"
-
+    
     @EnvironmentObject var database: Database
     @Environment(\.modelContext) var modelContext
     @AppStorage("isWelcomeScreenOver") var isWelcomeScreenOver = false
     @State var isShowingMain: Bool = false
     @State var showLogin: Bool = false
     @State private var isUserSignedIn: Bool = false
-
+    
     var fcmToken: String? {
         UserDefaults.standard.string(forKey: "fcmToken")
     }
-
+    
     private func generateUniqueCode() -> String {
         var code = ""
         repeat {
@@ -38,21 +38,21 @@ struct SignupView: View {
         } while false
         return code
     }
-
+    
     @State private var email: String = ""
     @State private var password: String = ""
     @AppStorage("uid") var userID: String = ""
     //@Binding var currentShowingView: String
     @Environment(\.colorScheme) var colorScheme
-
+    
     private func isValidPassword(_ password: String) -> Bool {
         let passwordRegex = NSPredicate(format: "SELF MATCHES %@", "^(?=.[a-z])(?=.[$@$#!%?&])(?=.[A-Z]).{6,}$")
         return passwordRegex.evaluate(with: password)
     }
-var body: some View {
+    var body: some View {
         ZStack {
             Color(colorScheme == .dark ? .black : .white)
-
+            
             VStack {
                 HStack {
                     Text("Create an Account!")
@@ -63,32 +63,32 @@ var body: some View {
                 }
                 .padding()
                 .padding(.top)
-
+                
                 Spacer()
                 HStack {
                     TextField("Name", text: $name)
                         .accessibilityRemoveTraits(.isStaticText)
                 }.foregroundStyle(colorScheme == .dark ? .white : .black)
-                .padding()
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(lineWidth: 2)
-                        .foregroundStyle(colorScheme == .dark ? .white : .black)
-                )
-                .padding()
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(lineWidth: 2)
+                            .foregroundStyle(colorScheme == .dark ? .white : .black)
+                    )
+                    .padding()
                 HStack {
                     TextField("Surname", text: $surname)
                         .accessibilityRemoveTraits(.isStaticText)
                 }.foregroundStyle(colorScheme == .dark ? .white : .black)
-                .padding()
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(lineWidth: 2)
-                        .foregroundStyle(colorScheme == .dark ? .white : .black)
-
-                )
-                .padding()
-HStack {
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(lineWidth: 2)
+                            .foregroundStyle(colorScheme == .dark ? .white : .black)
+                        
+                    )
+                    .padding()
+                HStack {
                     Image(systemName: "mail").foregroundStyle(colorScheme == .dark ? .white : .black)
                     TextField("Email", text: $email).foregroundStyle(colorScheme == .dark ? .white : .black)
                     if(email.count != 0) {
@@ -102,7 +102,7 @@ HStack {
                     RoundedRectangle(cornerRadius: 10)
                         .stroke(lineWidth: 2)
                         .foregroundStyle(colorScheme == .dark ? .white : .black)
-
+                    
                 )
                 .padding()
                 VStack(alignment: .leading, spacing: -8.0) {
@@ -119,17 +119,17 @@ HStack {
                                 .foregroundColor(isValidPassword(password) ? .green : .red)
                         }
                     }
-.padding()
+                    .padding()
                     .overlay(
                         RoundedRectangle(cornerRadius: 10)
                             .stroke(lineWidth: 2)
                             .foregroundStyle(colorScheme == .dark ? .white : .black)
-
+                        
                     )
-                .padding()
+                    .padding()
                 }
-
-
+                
+                
                 Button(action: {
                     withAnimation {
                         //self.currentShowingView = "login"
@@ -139,16 +139,10 @@ HStack {
                     Text("Already have an account?")
                         .foregroundStyle(colorScheme == .dark ? .white : .black)
                 }
-
+                
                 Spacer()
-Button {
-                    let user = User(name: name, surname: surname, phoneNumber: generatedCode, fcmToken: fcmToken ?? "")
-                    database.addUser(user: user, phoneNumber: user.phoneNumber)
-
-                    isWelcomeScreenOver = true
-                    isShowingMain.toggle()
-                    modelContext.insert(user)
-                    print("ciao")
+                Button {
+                    
                     Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
                         if let error = error {
                             print(error)
@@ -159,6 +153,13 @@ Button {
                             userID = authResult.user.uid
                         }
                     }
+                    let user = User(name: name, surname: surname, phoneNumber: generatedCode, fcmToken: fcmToken ?? "")
+                    database.addUser(user: user, phoneNumber: user.phoneNumber)
+                    
+                    isWelcomeScreenOver = true
+                    isShowingMain.toggle()
+                    modelContext.insert(user)
+                    print("ciao")
                 } label: {
                     Text("Create New Account")
                         .foregroundStyle(colorScheme == .dark ? .black : .white)
@@ -173,12 +174,12 @@ Button {
                         .padding(.horizontal)
                 }.padding(.top, 30)
                 
-                .disabled(name.trimmingCharacters(in: .whitespaces).isEmpty || surname.trimmingCharacters(in: .whitespaces).isEmpty||email.trimmingCharacters(in: .whitespaces).isEmpty||password.trimmingCharacters(in: .whitespaces).isEmpty||isValidPassword(password))
+                    .disabled(name.trimmingCharacters(in: .whitespaces).isEmpty || surname.trimmingCharacters(in: .whitespaces).isEmpty||email.trimmingCharacters(in: .whitespaces).isEmpty||password.trimmingCharacters(in: .whitespaces).isEmpty||isValidPassword(password))
                 Spacer()
             }
-
+            
         }
-.onAppear {
+        .onAppear {
             generatedCode = generateUniqueCode()
         }
         .fullScreenCover(isPresented: $isShowingMain, content: {
