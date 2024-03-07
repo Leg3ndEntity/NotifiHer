ae10ed98586eaf8bc9e182e0937caaeed3700078
andrearomy <andry21yt@gmail.com>
2024-03-07 09:44:55 +0100

    3

--- Toothless/Toothless/Componentes/FakeCallsViewModel.swift
+++ Toothless/Toothless/Componentes/FakeCallsViewModel.swift
@@ -8,11 +8,11 @@
 import Foundation
 class FakeCallsViewModel{
     var fakecall = [
-     FakeCall(situation: "Taxi/Uber", duration: "15 min", imagename: "Taxi", video: "VideoProva4"),
-     FakeCall(situation: NSLocalizedString("Public Transportation", comment: ""), duration: "10 min", imagename: "Public Transportation", video: "VideoProva4"),
-     FakeCall(situation: NSLocalizedString("Walking Alone", comment: ""), duration: "10 min", imagename: "Walking", video: "VideoProva4"),
-     FakeCall(situation: "Taxi/Uber", duration: "15 min", imagename: "Taxi", video: "VideoProva4"),
-     FakeCall(situation: NSLocalizedString("Public Transportation", comment: ""), duration: "10 min", imagename: "Public Transportation", video: "VideoProva4"),
-     FakeCall(situation: NSLocalizedString("Walking Alone", comment: ""), duration: "10 min", imagename: "Walking", video: "VideoProva4")
+     FakeCall(situation: "Taxi/Uber", duration: "15 min", imagename: "Taxi", video: "VideoProva3"),
+     FakeCall(situation: NSLocalizedString("Public Transportation", comment: ""), duration: "10 min", imagename: "Public Transportation", video: "VideoProva3"),
+     FakeCall(situation: NSLocalizedString("Walking Alone", comment: ""), duration: "10 min", imagename: "Walking", video: "VideoProva3"),
+     FakeCall(situation: "Taxi/Uber", duration: "15 min", imagename: "Taxi", video: "VideoProva3"),
+     FakeCall(situation: NSLocalizedString("Public Transportation", comment: ""), duration: "10 min", imagename: "Public Transportation", video: "VideoProva3"),
+     FakeCall(situation: NSLocalizedString("Walking Alone", comment: ""), duration: "10 min", imagename: "Walking", video: "VideoProva3")
     ]
 }
